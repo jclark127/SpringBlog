83fed920ee9094c887db80695e96f7c1b3a62fb5
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-04-18 14:34:19 -0500

    fixed controller annotation issue

--- src/main/java/com/codeup/springblog/MathControllerClass.java
+++ src/main/java/com/codeup/springblog/MathControllerClass.java
@@ -1,7 +1,9 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class MathControllerClass {
 
     @RequestMapping(path = "/add/{n1}/and/{n2}", method = RequestMethod.GET)
@@ -10,19 +12,19 @@ public class MathControllerClass {
         return n1 + n2;
     }
 
-    @RequestMapping(path = "/add/{n1}/and/{n2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/subtract/{n1}/and/{n2}", method = RequestMethod.GET)
     @ResponseBody
     public int subtract(@PathVariable int n1, @PathVariable int n2){
         return n1 - n2;
     }
 
-    @RequestMapping(path = "/add/{n1}/and/{n2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/multiply/{n1}/and/{n2}", method = RequestMethod.GET)
     @ResponseBody
     public int multiply(@PathVariable int n1, @PathVariable int n2){
         return n1 * n2;
     }
 
-    @RequestMapping(path = "/add/{n1}/and/{n2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/divide/{n1}/and/{n2}", method = RequestMethod.GET)
     @ResponseBody
     public int divide(@PathVariable int n1, @PathVariable int n2){
         return n1 / n2;
