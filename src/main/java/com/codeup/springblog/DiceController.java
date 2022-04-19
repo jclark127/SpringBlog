22442d05ee334f16faa690d9978cb4ec3223eea8
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-04-19 12:31:02 -0500

    working through view exercises

---
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -0,0 +1,23 @@
+package com.codeup.springblog;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+
+        return "rollDice";
+    }
+    @GetMapping("/roll-dice/{pick}")
+    public String guesser(@PathVariable Integer pick, Model model) {
+        model.addAttribute("pick", pick);
+        model.addAttribute("random", (int)(Math.random()*((6-1)+1))+1);
+        return "rollDice";
+    }
+
+}
